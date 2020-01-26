07042968ea5fcdf82181b43f66161199c8d7ab2e
saber2pr <saber2pr@gmail.com>
2020-01-26 21:28:20 +0000

    feat(*): update

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * @Author: saber2pr
  * @Date: 2020-01-26 20:59:24
  * @Last Modified by: saber2pr
- * @Last Modified time: 2020-01-26 21:00:37
+ * @Last Modified time: 2020-01-26 21:27:09
  */
 var LOADING = (() => {
-  let LoadingStyle = `.Loading{position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);}.Loading-Block{width:24px;height:24px;background:#a2b4cf;border-radius:100%;display:inline-block;animation:slide 1s infinite;}.Loading-Block:nth-child(1){animation-delay:0.1s;background:#8695ac;}.Loading-Block:nth-child(2){animation-delay:0.2s;background:#6e7a8d;}.Loading-Block:nth-child(3){animation-delay:0.3s;background:#555e6d;}.Loading-Block:nth-child(4){animation-delay:0.4s;background:#3e4550;}.Loading-Block:nth-child(5){animation-delay:0.5s;background:#262a31;}@-moz-keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}@-webkit-keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}@-o-keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}@keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}`
+  let LoadingStyle = `.Loading{position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);}.Loading-Block{width:24px;height:24px;background:#a2b4cf;border-radius:100%;display:inline-block;animation:slide 1s infinite;}.Loading-Block:nth-child(1){animation-delay:0.1s;background:#8695ac;}.Loading-Block:nth-child(2){animation-delay:0.2s;background:#6e7a8d;}.Loading-Block:nth-child(3){animation-delay:0.3s;background:#555e6d;}.Loading-Block:nth-child(4){animation-delay:0.4s;background:#3e4550;}.Loading-Block:nth-child(5){animation-delay:0.5s;background:#262a31;}@-moz-keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}@-webkit-keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}@-o-keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}@keyframes slide{0%{transform:scale(1);}50%{opacity:0.3;transform:scale(2);}100%{transform:scale(1);}}`
   let Loading = `<div class="Loading"><div class="Loading-Block"></div><div class="Loading-Block"></div><div class="Loading-Block"></div><div class="Loading-Block"></div><div class="Loading-Block"></div></div>`
 
   let style = document.createElement("style")
